65a3924aa9431574bd4287b7cac0c4b88a3ad5f3
Matheo HERSAN <mahersan>
2023-04-05 13:40:33 +0200

    Added setting page

---
+++ MangaMap/Views/settingsPage.xaml.cs
@@ -0,0 +1,14 @@
+namespace MangaMap.Views;
+
+public partial class settingsPage : ContentPage
+{
+	public settingsPage()
+	{
+		InitializeComponent();
+	}
+
+    private void OnLoginClicked(object sender, EventArgs e)
+    {
+        //
+    }
+}
