e34dcf20c22e1fcb7cc5f7d24a195261026f0ff2
V J <Vianney.JOURDY@etu.uca.fr>
2023-05-11 17:50:47 +0200

    modification navigation

--- MangaMap/CustomHeader.xaml.cs
+++ MangaMap/CustomHeader.xaml.cs
@@ -11,5 +11,11 @@ public partial class NewContent1 : ContentView
 	void ImageButton_Clicked(System.Object sender, System.EventArgs e)
 	{
         Navigation.PushAsync(new homePage());
+        //ShellContent(new homePage());
     }
+
+	void SettingButton_Clicked(object sender, System.EventArgs e)
+	{
+		Navigation.PushAsync(new settingsPage());
+	}
 }
