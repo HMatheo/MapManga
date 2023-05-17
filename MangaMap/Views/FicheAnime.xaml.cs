447becc32be1d7928d6f3be3331c98000737335e
Matheo HERSAN <mahersan>
2023-05-17 17:19:47 +0200

    Beginning of the DataBinding

--- MangaMap/Views/FicheAnime.xaml.cs
+++ MangaMap/Views/FicheAnime.xaml.cs
@@ -3,15 +3,13 @@ using Model;
 
 public partial class ficheAnime : ContentPage
 {
-    public Oeuvre AnimeModel { get; set; }
+    public Manager DataManager { get; set; }
 
     public ficheAnime()
     {
         InitializeComponent();
 
-        // Exemple de création d'une instance de la classe Oeuvre
-        List<string> genres = new List<string>() { "Action", "Aventure" };
-        AnimeModel = new Oeuvre("Nom de l'oeuvre", genres, "Type de l'oeuvre", "Description de l'oeuvre", 5, 12, "Chemin/vers/l'affiche.png");
+        DataManager = new Manager();
 
         this.BindingContext = this;
     }
