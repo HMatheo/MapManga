6e88c66e20b1e3263147a4eb2126cf9beed6aee0
Matheo HERSAN <mahersan>
2023-05-22 11:51:39 +0200

    Continue DataBinding

---
+++ MangaMap/DataBinding/INotifyPropertyChanged.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangaMap.DataBinding
+{
+    public interface INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
--- MangaMap/Views/FicheAnime.xaml.cs
+++ MangaMap/Views/FicheAnime.xaml.cs
@@ -1,18 +1,37 @@
 namespace MangaMap.Views;
 using Model;
+using System.ComponentModel;
 
-public partial class ficheAnime : ContentPage
+public partial class ficheAnime : ContentPage, INotifyPropertyChanged
 {
-    public Oeuvre AnimeModel { get; set; }
+    private Oeuvre animeModel;
+    public Oeuvre AnimeModel
+    {
+        get { return animeModel; }
+        set
+        {
+            if (animeModel != value)
+            {
+                animeModel = value;
+                OnPropertyChanged(nameof(AnimeModel));
+            }
+        }
+    }
 
     public ficheAnime()
     {
+        List<string> genres = new List<string>() { "Action", "Aventure" };
+        AnimeModel = new Oeuvre("Evangelion", genres, "Type de l'oeuvre", "Description de l'oeuvre", 5, 12, "Chemin/vers/l'affiche.png");
+
         InitializeComponent();
 
-        // Exemple de création d'une instance de la classe Oeuvre
-        List<string> genres = new List<string>() { "Action", "Aventure" };
-        AnimeModel = new Oeuvre("Nom de l'oeuvre", genres, "Type de l'oeuvre", "Description de l'oeuvre", 5, 12, "Chemin/vers/l'affiche.png");
-
         this.BindingContext = this;
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
