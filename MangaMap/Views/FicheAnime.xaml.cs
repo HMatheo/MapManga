078130d768b38d4836c4af05bcfa3d1b0ded9830
Matheo HERSAN <mahersan>
2023-05-22 11:01:57 +0200

    Enhancement DataBinding

--- MangaMap/Views/FicheAnime.xaml.cs
+++ MangaMap/Views/FicheAnime.xaml.cs
@@ -3,13 +3,15 @@ using Model;
 
 public partial class ficheAnime : ContentPage
 {
-    public Manager DataManager { get; set; }
+    public Oeuvre AnimeModel { get; set; }
 
     public ficheAnime()
     {
         InitializeComponent();
 
-        DataManager = new Manager();
+        // Exemple de création d'une instance de la classe Oeuvre
+        List<string> genres = new List<string>() { "Action", "Aventure" };
+        AnimeModel = new Oeuvre("Nom de l'oeuvre", genres, "Type de l'oeuvre", "Description de l'oeuvre", 5, 12, "Chemin/vers/l'affiche.png");
 
         this.BindingContext = this;
     }
