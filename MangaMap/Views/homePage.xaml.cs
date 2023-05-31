0eb5cc11c412fa1faf109148977ac8e2c8893f9d
V J <Vianney.JOURDY@etu.uca.fr>
2023-05-31 16:35:17 +0200

    databinding homePage

--- MangaMap/Views/homePage.xaml.cs
+++ MangaMap/Views/homePage.xaml.cs
@@ -9,20 +9,60 @@ public partial class homePage : ContentPage
 
     public homePage()
 	{
-
         InitializeComponent();
 		BindingContext = my_manager;
-
+        chargerSerie();
 	}
 
     private async void AnimeImageClicked(object sender, EventArgs e)
     {
-        var selectedAnime = (sender as ImageButton)?.BindingContext as Oeuvre;
-        if (selectedAnime != null)
+        //var selectedAnime = (sender as ImageButton)?.BindingContext as Oeuvre;
+        //if (selectedAnime != null)
+        //{
+        //    // Naviguez vers la page de la fiche d'anime en passant l'objet sélectionné
+        //    await Navigation.PushAsync(new ficheAnime(selectedAnime));
+        //}
+
+        var button = (ImageButton)sender;
+        var idAutomation = button.AutomationId;
+
+        if (int.TryParse(idAutomation, out int id))
         {
-            // Naviguez vers la page de la fiche d'anime en passant l'objet sélectionné
-            await Navigation.PushAsync(new ficheAnime(selectedAnime));
+            await Navigation.PushAsync(new ficheAnime(my_manager.Oeuvres[id]));
         }
     }
+
+    private void chargerSerie()
+    {
+        int imagesParLigne = 4;
+        int indice = 0;
+
+        for (int i = 0; i < my_manager.Oeuvres.Count; i++)
+        {
+            Oeuvre favoris = my_manager.Oeuvres[i];
+
+            ImageButton imageButton = new ImageButton
+            {
+                Source = favoris.Affiche,
+                WidthRequest = 150,
+                HeightRequest = 170,
+                CornerRadius = 15,
+                Aspect = Aspect.Fill,
+                AutomationId = indice.ToString(),
+            };
+
+            imageButton.Clicked += AnimeImageClicked;
+
+            int ligne = 1 + (indice / imagesParLigne);
+            int colonne = indice % imagesParLigne;
+
+            Grid.SetRow(imageButton, ligne);
+            Grid.SetColumn(imageButton, colonne);
+            grille.Children.Add(imageButton);
+
+            indice++;
+        }
+    }
+
 }
 
--- MangaMap/Views/listPage.xaml.cs
+++ MangaMap/Views/listPage.xaml.cs
@@ -10,35 +10,5 @@ public partial class listPage : ContentPage
 	{
 		InitializeComponent();
         BindingContext = my_manager;
-        //if (my_manager.UtilisateurActuel.Email != null)
-        //{
-        //    chargerFavoris();
-        //}
-    }
-
-    private void chargerFavoris()
-    {
-        int indice = 0;
-
-        for (int i = 0; i < my_manager.UtilisateurActuel.ListeOeuvreEnVisionnage.Count; i++)
-        {
-            Oeuvre favoris = my_manager.UtilisateurActuel.ListeOeuvreEnVisionnage[i];
-
-            ImageButton imageButton = new ImageButton
-            {
-                Source = favoris.Affiche,
-                WidthRequest = 100,
-                HeightRequest = 100,
-            };
-
-            int ligne = 1 + indice;
-
-            Grid.SetRow(imageButton, ligne);
-            Grid.SetColumn(imageButton, 0);
-
-            //grille.Children.Add(imageButton);
-
-            indice++;
-        }
     }
 }
