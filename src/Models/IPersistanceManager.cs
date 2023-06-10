71b46f329424ec453db5be46731bdd840188735c
HMatheo <hersan.matheo@gmail.com>
2023-06-10 20:53:01 +0200

    Better documentation in the Models

--- src/Models/IPersistanceManager.cs
+++ src/Models/IPersistanceManager.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿///// \brief Fichier pour la classe Liste
+///// \author HERSAN Mathéo, JOURDY Vianney
+/// \file IPersistanceManager.cs
+
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
--- src/Models/Liste.cs
+++ src/Models/Liste.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿///// \brief Fichier pour la classe Liste
+///// \author HERSAN Mathéo, JOURDY Vianney
+/// \file Liste.cs
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
